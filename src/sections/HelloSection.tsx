c39b163d5ccc23f503df98c448b81e3ce258d189
rb3198 <ronitbhatia@hotmail.com>
2023-10-13 13:44:17 -0400

    10: Integration with Hello Section

--- src/sections/HelloSection.tsx
+++ src/sections/HelloSection.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import styles from "../styles/scss/hello_section.scss";
+import { Typing } from "rb3198/components/Typing";
+import { DETAILED_DESC, ONE_LINERS } from "rb3198/constants/";
 
 interface HelloSectionProps {}
 
@@ -9,8 +11,19 @@ export const HelloSection: React.FC<HelloSectionProps> = (props) => {
       <p className={styles.subtext}>Hello there, I'm</p>
       <p className={styles.title}>Ronit.</p>
       <p className={styles.oneLineDesc}>
-        I'm a <span>Web Developer.</span>
+        I'm{" "}
+        <Typing
+          displayDuration={2}
+          eraseDuration={2}
+          typingDuration={2}
+          strings={ONE_LINERS}
+          classes={styles.oneLiners}
+        />
       </p>
+      <p
+        className={styles.detailedDescription}
+        dangerouslySetInnerHTML={{ __html: DETAILED_DESC }}
+      ></p>
     </div>
   );
 };
