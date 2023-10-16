071da5090e27898ecbd78a975cef8f35ac6bce38
rb3198 <ronitbhatia@hotmail.com>
2023-10-16 15:27:25 -0400

    4: Make Header Navigation functional Scroll to ID on click of a nav component

--- src/components/HeaderNavigation.tsx
+++ src/components/HeaderNavigation.tsx
@@ -1,26 +1,58 @@
 import { HeaderOption } from "rb3198/types/enum/HeaderOption";
 import React, { useCallback } from "react";
 import styles from "rb3198/styles/scss/header_navigation.scss";
+import { HashLink } from "react-router-hash-link";
+import { useLocation } from "react-router-dom";
 
 interface HeaderNavigationProps {}
 
 const OPTIONS: HeaderOption[] = Object.values(HeaderOption);
 
 export const HeaderNavigation: React.FC<HeaderNavigationProps> = () => {
-  const navigate = useCallback((option: HeaderOption) => {
-    switch (option) {
-      case HeaderOption.Hello:
-        break;
-      default:
-        break;
-    }
-  }, []);
+  const location = useLocation();
+  const navigate: React.MouseEventHandler<HTMLAnchorElement> = useCallback(
+    (e) => {
+      const { target } = e;
+      const option = (target as HTMLAnchorElement).getAttribute("data-section");
+      let helloOption = document.querySelector(`[data-section="Hello"]`);
+      if (option === HeaderOption.Hello) {
+        e.preventDefault();
+        window.history.pushState({}, "", `/#${option}`);
+        window.scrollTo({ behavior: "smooth", top: 0 });
+        let prevSelectedOption = document.querySelector(
+          `[data-selected="true"]`
+        );
+        prevSelectedOption?.setAttribute("data-selected", "false");
+        helloOption?.setAttribute("data-selected", "true");
+      } else {
+        helloOption?.setAttribute("data-selected", "false");
+      }
+    },
+    []
+  );
 
   return (
     <nav>
       <ul className={styles.navigationList}>
         {OPTIONS.map((option) => {
-          return <li onClick={navigate.bind(null, option)}>{option}</li>;
+          let isSelected = false;
+          if (!location.hash) {
+            isSelected = option === HeaderOption.Hello;
+          } else {
+            isSelected = location.hash === `#${option}`;
+          }
+          return (
+            <li>
+              <HashLink
+                to={`#${option}`}
+                data-section={option}
+                onClick={navigate}
+                data-selected={isSelected}
+              >
+                {option}
+              </HashLink>
+            </li>
+          );
         })}
       </ul>
     </nav>
