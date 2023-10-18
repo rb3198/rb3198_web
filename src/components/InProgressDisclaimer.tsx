bfa5198dc8a5c370dd8452d0bc2acecdac438e82
rb3198 <ronitbhatia@hotmail.com>
2023-10-18 12:57:08 -0400

    Fix In progress disclaimer

--- src/components/InProgressDisclaimer.tsx
+++ src/components/InProgressDisclaimer.tsx
@@ -1,18 +1,10 @@
 import React from "react";
 import { REPO_LINK } from "rb3198/constants";
+import styles from "rb3198/styles/scss/in_progress.scss";
 
 export const InProgressDisclaimer: React.FC<{}> = () => {
   return (
-    <p
-      style={{
-        position: "absolute",
-        bottom: "20px",
-        left: "50%",
-        transform: "translateX(-50%)",
-        fontSize: "18px",
-        fontFamily: "Raleway",
-      }}
-    >
+    <p className={styles.inProgress}>
       Website Currently under development. Repository for this site can be found{" "}
       <a href={REPO_LINK} target="_blank">
         here
