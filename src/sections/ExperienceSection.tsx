22e908cdbd43865a746f44059d4d890ae996a0f4
rb3198 <ronitbhatia@hotmail.com>
2023-10-30 23:32:30 -0400

    4: Integrate with Experience Section

--- src/sections/ExperienceSection.tsx
+++ src/sections/ExperienceSection.tsx
@@ -3,6 +3,8 @@ import styles from "../styles/scss/sections/experience_section.scss";
 import { Section } from "./Section";
 import { Sections } from "rb3198/types/enum/Sections";
 import { InProgressDisclaimer } from "rb3198/components/InProgressDisclaimer";
+import { Timeline } from "rb3198/components/Timeline";
+import { timelineContent } from "rb3198/constants/contents";
 
 interface ExperienceSectionProps {}
 
@@ -13,6 +15,7 @@ export const ExperienceSection: React.FC<ExperienceSectionProps> = (props) => {
       classes={styles.experienceSection}
       title={"Experience"}
     >
+      <Timeline content={timelineContent} />
       <InProgressDisclaimer />
     </Section>
   );
