9fc63fd4815ff98aed53b2f6ce2e038da8b6e78a
rb3198 <ronitbhatia@hotmail.com>
2023-11-03 11:03:47 -0400

    6: Contact links in Contact section

--- src/constants/contact.ts
+++ src/constants/contact.ts
@@ -2,11 +2,13 @@ import {
   PiGithubLogoDuotone,
   PiLinkedinLogoDuotone,
   PiEnvelopeDuotone,
+  PiPhoneDuotone,
 } from "react-icons/pi";
 
 export const LINKED_IN_URL =
   "https://www.linkedin.com/in/ronit-bhatia-4507a6190/";
 export const GIT_URL = "https://github.com/rb3198/";
+export const CT_GIT_URL = "https://github.com/cw-ronitbhatia/";
 export const SELF_REPO_URL = "https://github.com/rb3198/rb3198_web";
 
 export const EMAIL = "ronitbhatia98@gmail.com";
@@ -16,4 +18,32 @@ export const titleLinks = [
   { href: GIT_URL, icon: PiGithubLogoDuotone },
   { href: LINKED_IN_URL, icon: PiLinkedinLogoDuotone },
   { href: `mailto:${EMAIL}`, icon: PiEnvelopeDuotone },
+];
+
+export const contactSectionLinks = [
+  {
+    href: `tel:${PHONE}`,
+    icon: PiPhoneDuotone,
+    label: "Call",
+    value: PHONE,
+  },
+  { href: GIT_URL, icon: PiGithubLogoDuotone, label: "GitHub", value: GIT_URL },
+  {
+    href: CT_GIT_URL,
+    icon: PiGithubLogoDuotone,
+    label: "GitHub 2",
+    value: CT_GIT_URL,
+  },
+  {
+    href: LINKED_IN_URL,
+    icon: PiLinkedinLogoDuotone,
+    label: "LinkedIn",
+    value: LINKED_IN_URL,
+  },
+  {
+    href: `mailto:${EMAIL}`,
+    icon: PiEnvelopeDuotone,
+    label: "Email",
+    value: EMAIL,
+  },
 ];
