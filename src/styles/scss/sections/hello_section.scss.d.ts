8fdaa3647f9639aaf7aa7612c3f36ffb99c37bb4
rb3198 <ronitbhatia@hotmail.com>
2024-07-10 12:35:13 -0400

    Add DP to the website

--- src/styles/scss/sections/hello_section.scss.d.ts
+++ src/styles/scss/sections/hello_section.scss.d.ts
@@ -2,6 +2,11 @@
 // Please do not change this file!
 interface CssExports {
   'detailedDescription': string;
+  'dp': string;
+  'dp_container': string;
+  'dp_ring': string;
+  'dp_ring_2': string;
+  'expand_ring': string;
   'helloSection': string;
   'oneLineDesc': string;
   'oneLiners': string;
