33b21e2a8e4befd809571efa8633a298bae4295f
rb3198 <ronitbhatia@hotmail.com>
2024-07-10 20:15:25 -0400

    2: Smoother Type Animation

--- src/components/Typing.tsx
+++ src/components/Typing.tsx
@@ -28,77 +28,96 @@ interface TypingProps {
 export const Typing: React.FC<TypingProps> = (props) => {
   const { strings, typingDuration, eraseDuration, displayDuration, classes } =
     props;
-  const [activeStrIdx, setActiveStrIdx] = useState(0);
-  const [activeWord, setActiveWord] = useState("");
-  const [mode, setMode] = useState<"typing" | "erasing">("typing");
-  const typeTimeout = useRef<NodeJS.Timeout | null>(null);
+  const containerRef = useRef<HTMLSpanElement>(null);
+  const displayAnimationStart = useRef<number>(0);
+  const typeAnimationStart = useRef<number>(0);
+  const eraseAnimationStart = useRef<number>(0);
 
-  const typeLetter = () => {
-    const activeStr = strings[activeStrIdx];
-    if (activeWord.length === activeStr.length) {
-      // Full word typed. Proceed to erase.
-      setMode("erasing");
-      return;
-    }
-    setActiveWord(activeStr.slice(0, activeWord.length + 1));
-  };
-
-  const eraseLetter = () => {
-    const activeStr = strings[activeStrIdx];
-    if (activeWord.length === 0) {
-      // word cycle completed. Proceed to the next word.
-      setActiveStrIdx((activeStrIdx + 1) % strings.length);
-      setMode("typing");
-      return;
-    }
-    setActiveWord(activeStr.slice(0, activeWord.length - 1));
-  };
-
-  useEffect(() => {
-    const activeStr = strings[activeStrIdx];
-    if (mode === "typing") {
-      typeTimeout.current = setTimeout(
-        typeLetter,
-        (typingDuration * 1000) / activeStr.length
-      );
-    } else {
-      typeTimeout.current = setTimeout(() => {
-        typeTimeout.current = setTimeout(
-          eraseLetter,
-          (eraseDuration * 1000) / activeStr.length
+  const typeLetter = (
+    timestamp: number,
+    word: string,
+    typedToIdx: number,
+    wordIdx: number,
+    nWords: number
+  ) => {
+    if (typedToIdx === word.length) {
+      if (!displayAnimationStart.current) {
+        displayAnimationStart.current = timestamp;
+      }
+      const displayElapsed = timestamp - displayAnimationStart.current;
+      if (displayElapsed >= displayDuration * 1000) {
+        eraseAnimationStart.current = timestamp;
+        displayAnimationStart.current = 0;
+        requestAnimationFrame((t) =>
+          eraseLetter(t, word, typedToIdx - 1, wordIdx, nWords)
         );
-      }, displayDuration * 1000);
+      } else {
+        requestAnimationFrame((t) =>
+          typeLetter(t, word, typedToIdx, wordIdx, nWords)
+        );
+      }
+      return;
     }
-  }, [mode]);
+    if (containerRef.current) {
+      containerRef.current.innerText = word.slice(0, typedToIdx + 1);
+    }
+    if (
+      timestamp - typeAnimationStart.current >=
+      (typingDuration * 1000) / word.length
+    ) {
+      typeAnimationStart.current = timestamp;
+      typedToIdx++;
+    }
+    requestAnimationFrame((t) =>
+      typeLetter(t, word, typedToIdx, wordIdx, nWords)
+    );
+  };
+
+  const eraseLetter = (
+    timestamp: number,
+    word: string,
+    typedToIdx: number,
+    wordIdx: number,
+    nWords: number
+  ) => {
+    if (typedToIdx < 0) {
+      const newIdx = (wordIdx + 1) % nWords;
+      typeAnimationStart.current = timestamp;
+      requestAnimationFrame((t) =>
+        typeLetter(t, strings[newIdx], 0, newIdx, nWords)
+      );
+      return;
+    }
+    if (containerRef.current) {
+      containerRef.current.innerText = word.slice(
+        0,
+        Math.max(0, typedToIdx - 1)
+      );
+    }
+    if (
+      timestamp - eraseAnimationStart.current >=
+      (eraseDuration * 1000) / word.length
+    ) {
+      eraseAnimationStart.current = timestamp;
+      typedToIdx--;
+    }
+    requestAnimationFrame((t) =>
+      eraseLetter(t, word, typedToIdx, wordIdx, nWords)
+    );
+  };
 
   useEffect(() => {
-    const activeStr = strings[activeStrIdx];
-    if (mode === "typing") {
-      typeTimeout.current = setTimeout(
-        typeLetter,
-        (typingDuration * 1000) / activeStr.length
-      );
-    } else {
-      typeTimeout.current = setTimeout(
-        eraseLetter,
-        (eraseDuration * 1000) / activeStr.length
-      );
-    }
-  }, [activeWord]);
-
-  useEffect(() => {
-    // cleanup everything on unmount
-    return () => {
-      typeTimeout.current && clearTimeout(typeTimeout.current);
-    };
+    typeAnimationStart.current = 0;
+    typeLetter(0, strings[0], 0, 0, strings.length);
   }, []);
 
   if (strings.length === 0) {
     return null;
   }
   return (
-    <span className={classes}>
-      {activeWord} <span className={styles.cursor}>|</span>
+    <span>
+      <span ref={containerRef} className={classes}></span>
+      <span className={styles.cursor}>|</span>
     </span>
   );
 };
